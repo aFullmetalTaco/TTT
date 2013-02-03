bd7b3b8e679d3716f349fd4498954a7bc3ebde2a
aaronhatton <me@aaronhatton.co.uk>
2013-02-03 15:13:11 +0000

    Added color smokes woooot

---
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_redsmokegrenade/shared.lua
@@ -0,0 +1,40 @@
+
+if SERVER then
+   AddCSLuaFile( "shared.lua" )
+end
+   
+SWEP.HoldType			= "grenade"
+
+if CLIENT then
+   SWEP.PrintName = "Red Smoke"
+   SWEP.Slot = 3
+
+   SWEP.Icon = "VGUI/ttt/icon_nades"
+   
+   SWEP.EquipMenuData = {
+      type  = "item_weapon",
+      name  = "Red Smoke",
+      desc  = [[Red smoke to hide and kill people]]
+   };
+   
+end
+
+SWEP.Base				= "weapon_tttbasegrenade"
+
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+
+SWEP.WeaponID = AMMO_SMOKE
+SWEP.Kind = WEAPON_NADE
+SWEP.CanBuy = {ROLE_TRAITOR}
+
+SWEP.ViewModel			= "models/weapons/v_eq_smokegrenade.mdl"
+SWEP.WorldModel			= "models/weapons/w_eq_smokegrenade.mdl"
+SWEP.Weight			= 5
+SWEP.AutoSpawnable      = true
+-- really the only difference between grenade weapons: the model and the thrown
+-- ent.
+
+function SWEP:GetGrenadeName()
+   return "ttt_redsmoke"
+end
